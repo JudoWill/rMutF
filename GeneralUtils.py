3aa08373d7b7a425b1f7d9d559ac4330874b5c2f
Will Dampier <judowill@gmail.com>
2011-04-07 13:16:35 -0400

    added a slugify function for dealing with searches

--- GeneralUtils.py
+++ GeneralUtils.py
@@ -1,5 +1,7 @@
 from threading import Semaphore, Timer
-
+import re
+import os
+import unicodedata
 
 
 class TimedSemaphore():
@@ -20,3 +22,26 @@ class TimedSemaphore():
 
     def __exit__(self, typ, value, traceback):
         Timer(self.time, self.release).start()
+
+def touch(fname, times = None):
+    with file(fname, 'a'):
+        os.utime(fname, times)
+
+
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters, 
+    and converts spaces to hyphens.
+    
+    From Django's "django/template/defaultfilters.py".
+    """
+
+    _slugify_strip_re = re.compile(r'[^\w\s-]')
+    _slugify_hyphenate_re = re.compile(r'[-\s]+')
+
+    if not isinstance(value, unicode):
+        value = unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+    return _slugify_hyphenate_re.sub('-', value)
